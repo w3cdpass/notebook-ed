3b882e29aec1f7e39dbfd5cd584f293a0c42cd3d
Kupasva <130740583+Kupasva@users.noreply.github.com>
2024-07-17 14:28:59 +0530

    Initial commit with main project files

---
+++ py2nb/__init__.py
@@ -0,0 +1,5 @@
+from .converter import delete_file, convert_py_to_ipynb, process_directory
+
+"""
+py2nb package
+"""
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py2nb',
+    version='0.1.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'py2nb=py2nb.converter:main',
+        ],
+    },
+    install_requires=[
+        'nbformat',
+    ],
+    author='w3cdpass',
+    author_email='kupasva663@gmail.com',
+    description='A package to convert .py files to .ipynb and delete the original .py files.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/w3cdpass/py2nb.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
