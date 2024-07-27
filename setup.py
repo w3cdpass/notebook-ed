7b60547056b95f7349ae3c950728a9ab638a743e
Kupasva <130740583+Kupasva@users.noreply.github.com>
2024-07-27 13:50:05 +0530

    Main

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name='notebook-ed',
     version='0.1.1',
